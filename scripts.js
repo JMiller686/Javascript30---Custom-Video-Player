420efef7bfc25ed4c6d91713674809f538cd6346
JMiller686 <jmiller686@gmail.com>
2019-04-22 08:41:57 -0500

    play/pause video

--- scripts.js
+++ scripts.js
@@ -4,9 +4,30 @@ const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelector('[data-skip]');
-const ranges = play.querySelectorAll('.player__slider');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
 
 /* Build functions */
+function togglePlay() {
+    if(video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
+function updateButton() {
+    const icon = this.paused ? '►' : '❚❚';
+    toggle.textContent = icon;
+}
+
+function skip() {
+    console.log(`Skipping!`);
+}
 
 /* Hook up the event listeners */
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
